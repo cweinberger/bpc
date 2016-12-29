488a3fb68610d9bccff9bb6a16be77cbfe70ea28
Daniel Kokott <dako@berlingskemedia.dk>
2016-12-29 09:14:33 +0100

    OK this gulp might finally work

--- gulpfile.js
+++ gulpfile.js
@@ -6,49 +6,57 @@ var application;
 var consoleApp;
 var webpack;
 
-gulp.task('default', ['server', 'application', 'console', 'start_webpack']);
 
-gulp.task('start_server', function() {
-  if (server) {
-    server.kill();
-  }
-  server = spawn('node', ['./server/index.js'], {stdio: 'inherit'});
-});
+gulp.task('default', ['webpack_watch', 'watch'], function(){
 
-gulp.task('server', ['start_server'], function (done) {
-  gulp.watch(['./server/*.js'], ['start_server']);
+  start_server();
 
   setTimeout(function() {
-    done();
+    start_console();
+    start_application();
   }, 1000);
 });
 
-gulp.task('start_application', function() {
-  if (application) {
-    application.kill();
-  }
-  application = spawn('node', ['./application/index.js'], {stdio: 'inherit'});
-});
 
-gulp.task('application', ['start_application'], function () {
-  gulp.watch(['./application/*.js'], ['start_application']);
-});
+gulp.task('start_server', start_server);
+gulp.task('start_application', start_application);
+gulp.task('start_console', start_console);
 
 
-gulp.task('start_console', function() {
-  if (consoleApp) {
-    consoleApp.kill();
-  }
-  consoleApp = spawn('node', ['./console/index.js'], {stdio: 'inherit'});
-});
-
-gulp.task('console', ['start_console'], function () {
-  gulp.watch(['./console/*.js'], ['start_console']);
-});
-
-gulp.task('start_webpack', function() {
+gulp.task('webpack_watch', function() {
   if (webpack) {
     webpack.kill();
   }
   webpack = spawn('webpack', ['--watch'], {stdio: 'inherit'});
 });
+
+
+gulp.task('watch', function(){
+  gulp.watch(['./application/*.js'], ['start_application']);
+  gulp.watch(['./server/*.js'], ['start_server']);
+  gulp.watch(['./console/*.js'], ['start_console']);
+});
+
+
+function start_server(){
+  if (server) {
+    server.kill();
+  }
+  server = spawn('node', ['./server/index.js'], {stdio: 'inherit'});
+}
+
+
+function start_console(){
+  if (consoleApp) {
+    consoleApp.kill();
+  }
+  consoleApp = spawn('node', ['./console/index.js'], {stdio: 'inherit'});
+}
+
+
+function start_application(){
+  if (application) {
+    application.kill();
+  }
+  application = spawn('node', ['./application/index.js'], {stdio: 'inherit'});
+}

31f05adec3a56f94af59e8f4d189ae98a5ee3b0d
Daniel Kokott <dako@berlingskemedia.dk>
2017-03-07 11:07:55 +0100

    Not needing /validate

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const Rsvp = require('./rsvp');
 const OzLoadFuncs = require('./oz_loadfuncs');
 const Applications = require('./applications');
 const Users = require('./users');
-const Validate = require('./validate');
 const Permissions = require('./permissions');
 const Settings = require('./settings');
 const Scarecrow = require('scarecrow');
@@ -41,7 +40,6 @@ server.register(Scarecrow, function(err) {
   server.register(Rsvp, { routes: { prefix: '/rsvp' } }, cb);
   server.register(Applications, { routes: { prefix: '/applications' } }, cb);
   server.register(Users, { routes: { prefix: '/users' } }, cb);
-  server.register(Validate, { routes: { prefix: '/validate' } }, cb);
   server.register(Permissions, { routes: { prefix: '/permissions' } }, cb);
   server.register(Settings, { routes: { prefix: '/settings' } }, cb);
 });

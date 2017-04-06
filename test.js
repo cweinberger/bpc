b50c7106973e88a08e192eafcab719bc8a4f5bc7
Martin Kock <mkoc@berlingskemedia.dk>
2017-04-06 16:07:04 +0200

    Whitespace

--- test.js
+++ test.js
@@ -2,31 +2,31 @@ var request = require("request");
 
 var options = { method: 'POST',
   url: 'https://accounts.eu1.gigya.com/accounts.initRegistration',
-  qs: 
+  qs:
    { apiKey: '3_Pax_srwZ6L6thNGQ4d8nFGSnCU5AxIrsQo6oR0SQD-yYibdLPKCPl_iZiWVr_0Wu',
      userKey: 'AHOCBBStw4BC',
      secret: 'S13yBuUCksdJRvq87BQxcYqzmNCmOyme' },
-  headers: 
+  headers:
    { 'postman-token': 'ffe8c657-9f11-e11a-3b82-ec0761856fc4',
      'cache-control': 'no-cache',
      'content-type': 'application/json' } };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-  
+
   body = JSON.parse(body);
   console.log(body);
 
   var options = { method: 'POST',
     url: 'https://accounts.eu1.gigya.com/accounts.register',
-    qs: 
+    qs:
     { apiKey: '3_Pax_srwZ6L6thNGQ4d8nFGSnCU5AxIrsQo6oR0SQD-yYibdLPKCPl_iZiWVr_0Wu',
       userKey: 'AHOCBBStw4BC',
       secret: 'S13yBuUCksdJRvq87BQxcYqzmNCmOyme' },
-    headers: 
+    headers:
     { 'cache-control': 'no-cache',
       'content-type': 'application/x-www-form-urlencoded' },
-    form: 
+    form:
     { email: 'mkoc3@berlingskemedia.dk',
       password: 'Gigya123',
       profile: '{"firstName": "Martin", "lastName": "Kock"}',
@@ -38,5 +38,5 @@ request(options, function (error, response, body) {
 
     console.log(body);
   });
-  
+
 });

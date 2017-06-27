dc9ee05032282fc8d86b0319261cc34daf8a9aa3
Daniel Kokott <dako@berlingskemedia.dk>
2017-06-27 15:27:52 +0200

    console tests

--- test/bpc_helper.js
+++ test/bpc_helper.js
@@ -1,29 +1,17 @@
 const MongoDB = require('./../server/mongo/mongodb_mocked');
 const Hawk = require('hawk');
+const Oz = require('oz');
 const bpc = require('./../server');
+const crypto = require('crypto');
 
 
-module.exports.request = function (options, ticket, callback) {
-
-  const hawkHeader = Hawk.client.header('http://test.com'.concat(options.url), options.method, ticket);
-  if (!hawkHeader.field){
-    callback(hawkHeader);
-  }
-
-  const req = {
-    method: options.method,
-    url: options.url,
-    headers: {
-      host: 'test.com',
-      authorization: hawkHeader.field
-    }
-  };
-
-  bpc.inject(req, callback);
-};
-
-
-module.exports.apps = {
+module.exports.apps = apps = {
+  console: {
+    id: 'console',
+    scope: ['admin', 'admin:*', 'admin:console'],
+    key: 'j4h2kj4h32lkh432lkh4dk32ljh4lk32djh4lkj32h4',
+    algorithm: 'sha256'
+  },
   bt: {
     id: 'bt',
     scope: ['bt'],
@@ -38,7 +26,7 @@ module.exports.apps = {
   }
 };
 
-module.exports.users = {
+module.exports.users = users = {
   simple_first_user: {
     email: 'user@berlingskemedia.dk',
     id: '3218736128736123215732',
@@ -55,8 +43,74 @@ module.exports.users = {
       }
     },
     providerData: {}
+  },
+  console_superadmin_google_user: {
+    email: 'console_admin@berlingskemedia.dk',
+    id: '1111111111111111111111',
+    provider: 'google',
+    lastLogin: new Date(),
+    dataScopes: {},
+    providerData: {}
+  },
+  console_google_user: {
+    email: 'console_user@berlingskemedia.dk',
+    id: '2222222222222222222222',
+    provider: 'google',
+    lastLogin: new Date(),
+    dataScopes: {},
+    providerData: {}
   }
 };
+
+
+module.exports.grants = grants = {
+  console_superadmin_google_user__console_grant : {
+    id : '7462ydu3jjj3u32uej3mmsi3',
+    app : apps.console.id,
+    user : users.console_superadmin_google_user.id,
+    scope : ['admin:*'],
+    exp : null,
+    createdAt: new Date()
+  },
+  console_google_user__console_grant : {
+    id : '7362ydu3kkk3u65uej3mmsi4',
+    app : apps.console.id,
+    user : users.console_google_user.id,
+    scope : [],
+    exp : null,
+    createdAt: new Date()
+  }
+};
+
+
+module.exports.request = function (options, ticket, callback) {
+
+  const req = {
+    method: options.method,
+    url: options.url,
+    payload: options.payload,
+    headers: {
+      host: 'test.com'
+    }
+  };
+
+  if (ticket !== undefined && ticket !== null && ticket !== {}) {
+    const hawkHeader = Hawk.client.header('http://'.concat(req.headers.host, options.url), options.method, ticket);
+    if (!hawkHeader.field){
+      callback(hawkHeader);
+    }
+
+    req.headers.authorization = hawkHeader.field;
+  }
+
+  bpc.inject(req, callback);
+};
+
+
+module.exports.generateRsvp = function(app, grant, callback) {
+  Oz.ticket.rsvp(app, grant, process.env.ENCRYPTIONPASSWORD, {}, callback);
+};
+
 
 
 module.exports.initate = function (done) {
@@ -87,8 +141,9 @@ function clearMongoMock (done){
 function fillMongoMock (done){
   Promise.all([
     // Give the test cases a user to use.
-    MongoDB.collection('applications').insert(objectToArray(module.exports.apps)),
-    MongoDB.collection('users').insert(objectToArray(module.exports.users)),
+    MongoDB.collection('applications').insert(objectToArray(apps)),
+    MongoDB.collection('users').insert(objectToArray(users)),
+    MongoDB.collection('grants').insert(objectToArray(grants)),
   ]).then(res => {
     done();
   });
@@ -98,4 +153,9 @@ function fillMongoMock (done){
       return input[key];
     });
   }
+}
+
+
+function random40Character() {
+  return crypto.randomBytes(20).toString('hex'); // (gives 40 characters)
 }
---
+++ test/console_permissions_tests.js
@@ -0,0 +1,99 @@
+/* jshint node: true */
+'use strict';
+
+// Bootstrap the testing harness.
+const Code = require('code');   // assertion library
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const rewire = require('rewire');
+const sinon = require('sinon');
+const bpc_helper = require('./bpc_helper');
+// const Permissions = require('./../server/permissions');
+
+// Test shortcuts.
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+const before = lab.before;
+const after = lab.after;
+
+
+describe('console - functional tests:', () => {
+
+  var console_app = bpc_helper.apps.console;
+  var console_app_ticket;
+
+  before(done => {
+    bpc_helper.initate(function(){
+      done();
+    });
+  });
+
+  // Getting the console_app_ticket
+  before((done) => {
+    bpc_helper.request({ method: 'POST', url: '/ticket/app' }, {credentials: console_app}, (response) => {
+      expect(response.statusCode).to.equal(200);
+      console_app_ticket = {credentials: JSON.parse(response.payload), app: console_app.id};
+      done();
+    });
+  });
+
+
+
+  describe('superadmin', () => {
+
+    var console_superadmin_google_user = bpc_helper.users.console_superadmin_google_user;
+    var console_superadmin_google_user__console_grant = bpc_helper.grants.console_superadmin_google_user__console_grant;
+    var console_superadmin_google_user__console_rsvp;
+    var console_superadmin_google_user__console_ticket;
+
+    // User Getting the rsvp
+    before((done) => {
+      bpc_helper.generateRsvp(console_app, console_superadmin_google_user__console_grant, function(err, rsvp) {
+        console_superadmin_google_user__console_rsvp = rsvp;
+        done();
+      });
+    });
+
+    it('getting superadmin user ticket', (done) => {
+      bpc_helper.request({ method: 'POST', url: '/ticket/user', payload: { rsvp: console_superadmin_google_user__console_rsvp } }, console_app_ticket, (response) => {
+        expect(response.statusCode).to.equal(200);
+        console_superadmin_google_user__console_ticket = JSON.parse(response.payload);
+        var scope = console_superadmin_google_user__console_ticket.scope;
+        expect(scope).to.be.an.array();
+        expect(scope).to.include('admin');
+        expect(scope).to.include('admin:*');
+        done();
+      });
+    });
+  });
+
+
+
+  describe('non superadmin', () => {
+    var console_google_user = bpc_helper.users.console_google_user;
+    var console_google_user__console_grant = bpc_helper.grants.console_google_user__console_grant;
+    var console_google_user_rsvp;
+    var console_google_user_ticket;
+
+    // User Getting the rsvp
+    before((done) => {
+      bpc_helper.generateRsvp(console_app, console_google_user__console_grant, function(err, rsvp) {
+        console_google_user_rsvp = rsvp;
+        done();
+      });
+    });
+
+    it('getting user ticket', (done) => {
+      bpc_helper.request({ method: 'POST', url: '/ticket/user', payload: { rsvp: console_google_user_rsvp } }, console_app_ticket, (response) => {
+        expect(response.statusCode).to.equal(200);
+        console_google_user_ticket = JSON.parse(response.payload);
+        var scope = console_google_user_ticket.scope;
+        expect(scope).to.be.an.array();
+        expect(scope).to.include('admin');
+        expect(scope).to.not.include('admin:*');
+        done();
+      });
+    });
+  });
+});

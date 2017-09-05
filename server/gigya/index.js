bef5cb7dfa9fc3c849c5bf475f278bbc3d86fbeb
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2017-09-05 15:34:37 +0200

    Revert "Remove /gigya/search + /gigya/exists"
    This reverts commit 462bcfe2c13c8ff1ae5474a286953e652f1d94e6.

--- server/gigya/index.js
+++ server/gigya/index.js
@@ -13,6 +13,71 @@ const exposeError = GigyaUtils.exposeError;
 
 module.exports.register = function (server, options, next) {
 
+  const stdCors = {
+    credentials: true,
+    origin: ['*'],
+    headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match'],
+    exposedHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 86400
+  };
+
+
+  /**
+   * GET /users/search
+   *
+   * Query parameters:
+   * - query=<Gigya SQL-style query> eg.;
+   *   SELECT * FROM accounts WHERE profile.email = "mkoc@berlingskemedia.dk"
+   */
+  server.route({
+    method: 'GET',
+    path: '/search',
+    config: {
+      auth: {
+        access: {
+          scope: ['admin', 'users'],
+          entity: 'any'
+        }
+      },
+      cors: stdCors
+    },
+    handler: (request, reply) => {
+      return GigyaAccounts.searchAccount(request.query.query)
+        .then(res => reply(res.body), err => exposeError(reply, err));
+    }
+  });
+
+
+  /**
+   * GET /users/exists
+   *
+   * Query parameters:
+   * - email=email to check
+   */
+  server.route({
+    method: 'GET',
+    path: '/exists',
+    config: {
+      auth: {
+        access: {
+          scope: ['admin', 'users'],
+          entity: 'any'
+        }
+      },
+      cors: stdCors,
+      validate: {
+        query: Joi.object().keys({
+          email: Joi.string().email()
+        }).unknown(false)
+      }
+    },
+    handler: (request, reply) => {
+      return Gigya.callApi('/accounts.isAvailableLoginID', {loginID: request.query.email})
+        .then(res => reply(res.body), err => exposeError(reply, err));
+    }
+  });
+
+
   server.route({
     method: 'POST',
     path: '/notifications',

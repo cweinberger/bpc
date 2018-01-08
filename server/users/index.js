d1b50f974b858fd248ad8cbab34e4ffd0a2256d3
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2018-01-08 11:30:30 +0100

    UTIL-2261 log of query

--- server/users/index.js
+++ server/users/index.js
@@ -251,11 +251,14 @@ module.exports.register = function (server, options, next) {
     handler: (request, reply) => {
 
       let user = request.payload;
+      console.log('user', user);
       const userQuery = {
         query: 'select UID from accounts where loginIDs.emails = "' + user.email + '" '
       };
+      console.log('userQuery', userQuery);
 
       Gigya.callApi('/accounts.search', userQuery).then(data => {
+        console.log('data', data.body);
         if (data.body.results === undefined || data.body.results.length === 0) {
           EventLog.logUserEvent(null, 'User not found', {email: user.email});
           return reply(Boom.notFound("User " + user.email + " not found"));
